19ae1c5ed79b2c39c2ffd8047f604912cece4bee
jabberwocky97 <114198365+jabberwocky97@users.noreply.github.com>
2022-11-19 02:39:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+"""
+Project Name: Kms_Vault
+Project Description: Write a python program that gets a message and encrypts/decrypts
+it using “aes256-gcm96”
+Programmer: Michaela Pierce
+Date: 11/17/2022
+UNCW, CSC 592 - Introduction to Cryptography
+"""
 
